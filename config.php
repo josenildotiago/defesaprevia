a0ac78792b7607c6d5255f9706da2e3397e5491d
Josenildo Tiago <josenildo.tiago.designer@gmail.com>
2019-03-31 20:16:21 -0300

    db-PDO mudanças

--- config.php
+++ config.php
@@ -4,22 +4,31 @@ require 'environment.php';
 $config = array();
 if(ENVIRONMENT == 'development') {
 	define("BASE_URL", "http://localhost/defesaprevia/");
-	$config['dbname'] = 'getranmc_cadastro_estacionamento';
-	$config['host'] = 'localhost';
-	$config['dbuser'] = 'root';
-	$config['dbpass'] = '';
+	$host = "localhost";
+	$user = "root";
+	$pass = "";
+	$db = "getranmc_cadastro_estacionamento";
+	$dsn = "mysql:host={$host};dbname={$db}";
+	$options = array(
+	PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING,
+	PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8");
 } else {
 	define("BASE_URL", "http://meusite.com.br/");
-	$config['dbname'] = 'getranmc_cadastro_estacionamento';
-	$config['host'] = 'localhost';
-	$config['dbuser'] = 'root';
-	$config['dbpass'] = 'root';
+	$host = "localhost";
+	$user = "root";
+	$pass = "";
+	$db = "getranmc_cadastro_estacionamento";
+	$dsn = "mysql:host={$host};dbname={$db}";
+	$options = array(
+	PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING,
+	PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8");
 }
 
 global $db;
-try {
-	$db = new PDO("mysql:dbname=".$config['dbname'].";host=".$config['host'], $config['dbuser'], $config['dbpass']);
-} catch(PDOException $e) {
-	echo "ERRO: ".$e->getMessage();
-	exit;
+try{
+	$db = new PDO($dsn,$user,$pass,$options);
+	//$this->pdo = new PDO("mysql:dbname=usuarios;host=localhost","root","");
+}
+catch(PDOException $e){
+	echo "ERRO AO CONECTAR AO BANCO, CAUSA: ". $e->getMessage();
 }
